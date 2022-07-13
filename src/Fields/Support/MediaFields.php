e71c8aeb33d16f5567134712d8e7d0b7b7173497
Alexander Wenzel <alexander.wenzel.berlin@gmail.com>
2022-07-13 20:13:17 +0200

    better naming for columns

--- src/Fields/Support/MediaFields.php
+++ src/Fields/Support/MediaFields.php
@@ -17,27 +17,7 @@ class MediaFields
         return function (Request $request) {
             return [
                 ID::make(),
-
-                Text::make(__('Media Filename'), 'file_name')
-                    ->rules('required', 'min:2'),
-
                 Textarea::make(__('Media Description'), 'custom_properties->description')->alwaysShow(),
-
-                Text::make(__('Media Disk'))->exceptOnForms(),
-
-                Text::make(__('Media Download Url'), function () {
-                    return $this->resource->exists ? $this->resource->getFullUrl() : null;
-                }),
-
-                Text::make(__('Media Size'))->displayUsing(function () {
-                    return $this->resource->humanReadableSize;
-                })->exceptOnForms(),
-
-                Text::make(__('Media Updated At'))->displayUsing(function () {
-                    return $this->resource->updated_at->diffForHumans();
-                })->exceptOnForms(),
-
-                GeneratedConversions::make(__('Media Conversions')),
             ];
         };
     }
